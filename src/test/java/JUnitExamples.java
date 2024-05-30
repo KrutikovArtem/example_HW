171aa39d4faead69dea4207b1e5f4fdf41a2eabf
Artem Krutikov <stealthy_mzk@mail.ru>
2024-05-30 16:44:53 +0700

    Селекторы.Работа в классе

---
+++ src/test/java/JUnitExamples.java
@@ -0,0 +1,20 @@
+
+
+public class JUnitExamples {
+
+
+//    @BeforeAll
+//    static void beforeAll(){
+//        System.out.println("Here is beforeAll");
+//    }
+//
+//    @Test
+//    void firstTest(){
+//        System.out.println("Here is firstTest()");
+//    }
+//
+//    @Test
+//    void secondTest(){
+//        System.out.println("Here is secondTest()");
+//    }
+}
---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,26 @@
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+  @Test
+  void fillFormTest(){
+    open("https://demoqa.com/text-box");
+
+    $("#userName").setValue("Alex Egorov");
+    $("#=userEmail").setValue("stealthy_mzk@mail.ru");
+    $("#=currentAddress").setValue("Mzk");
+    $("#=permanentAddress").setValue("Brabskaya 14");
+
+    $("#=submit").click();
+    $("#=output").shouldHave(
+            text("Alex Egorov"),
+            text("stealthy_mzk@mail.ru"),
+            text("Mzk"),
+            text("Brabskaya 14"));
+
+    }
+}
